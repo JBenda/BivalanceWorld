8f72f941c6df916d939e19fe1bb6fee93a96066f
Julian Benda <julian.benda@ovgu.de>
2021-04-10 22:48:05 +0200

    Able to parse and show a world
    in a fixed file

--- Field.cpp
+++ Field.cpp
@@ -1,30 +1,117 @@
 #include "Field.hpp"
 
+
+int ColorId = 0;
+
+namespace hide {
+	template<Size S, Form F = static_cast<Form>(0)>
+	const form_t& getFormForm(Form);
+}
+
+
+template<Size S = Size::BEGIN>
+const form_t&
+getForm(Size _s, Form _f) {
+	if constexpr (S != Size::END) {
+		if (S == _s) {
+			return hide::getFormForm<S>(_f);
+		} else {
+			return getForm<S+1>(_s, _f);
+		}
+	} else {
+		throw FormNotFoundException(_s, _f);
+	}
+}
+
+const form_t& findForm(Size _s, Form _f) {
+	return getForm(_s, _f);
+}
+
+namespace hide {
+	template<Size S, Form F>
+	extern const form_t form;
+
+	template<Size S, Form F>
+	const form_t&
+	getFormForm(Form _f) {
+		if constexpr (F != Form::END) {
+			if (_f == F) {
+				return form<S, F>;
+			} else {
+				return getFormForm<S,F+1>(_f);
+			}
+		} else {
+			throw FormNotFoundException(S, _f);
+		}
+	}
+}
 template<>
-form_t hide::form<Size::SMALL, Form::CUBE> =
+const form_t hide::form<Size::SMALL, Form::CUBE> =
 	"         "
-	"    XX   "
-	"    XX   "
+	"   XXX   "
+	"   XXX   "
 	"         ";
 
 template<>
-form_t hide::form<Size::MEDIUM, Form::CUBE> = 
+const form_t hide::form<Size::MEDIUM, Form::CUBE> = 
 	"   XXXX  "
 	"   XXXX  "
 	"   XXXX  "
 	"         ";
 
 template<>
-form_t hide::form<Size::LARGE, Form::CUBE> = 
+const form_t hide::form<Size::LARGE, Form::CUBE> = 
 	" XXXXXXX "
 	" XXXXXXX "
 	" XXXXXXX "
 	" XXXXXXX ";
 
+template<>
+const form_t hide::form<Size::SMALL, Form::TET> =
+	"         "
+	"    X    "
+	"   XXX   "
+	"         ";
+
+template<>
+const form_t hide::form<Size::MEDIUM, Form::TET> = 
+	"    X    "
+	"   XXX   "
+	"  XXXXX  "
+	"         ";
+
+template<>
+const form_t hide::form<Size::LARGE, Form::TET> = 
+	"    X    "
+	"   XXX   "
+	"  XXXXX  "
+	" XXXXXXX ";
+
+template<>
+const form_t hide::form<Size::SMALL, Form::DODEC> =
+	"         "
+	"    X    "
+	"    X    "
+	"         ";
+
+template<>
+const form_t hide::form<Size::MEDIUM, Form::DODEC> = 
+	"    X    "
+	"   XXX   "
+	"   XXX   "
+	"    X    ";
+
+template<>
+const form_t hide::form<Size::LARGE, Form::DODEC> = 
+	"   XXX   "
+	"  XXXXX  "
+	"  XXXXX  "
+	"   XXX   ";
+
 const char* toString(Size _s) {
 	switch (_s) {
 		case Size::SMALL: return "Small";
-		case Size::MEDIUM: return "Meduium";
+		case Size::MEDIUM: return "Medium";
 		case Size::LARGE: return "Large";
 		case Size::END: return nullptr;
 	}
@@ -40,3 +127,55 @@ const char* toString(Form _f) {
 	}
 	return nullptr;
 }
+
+std::array<int, static_cast<int>(Form::END)> FormColor;
+
+void initForm() {
+	auto itr = FormColor.begin();
+	init_pair(ColorId, COLOR_BLACK, COLOR_MAGENTA);
+	*itr++ = ColorId++;
+	init_pair(ColorId, COLOR_BLACK, COLOR_CYAN);
+	*itr++ = ColorId++;
+	init_pair(ColorId, COLOR_BLACK, COLOR_YELLOW);
+	*itr++ = ColorId++;
+}
+
+auto fetchField(const json& j, const char* _name) {
+	if (const auto itr = j.find(_name); itr != j.end()) {
+		return *itr;
+	} else {
+		throw FieldNotFound(_name);
+	}
+}
+
+Object::Object(const json& j) {
+	for (const auto& s : fetchField(j, "Consts")) {
+		m_symbols.set(getSymbolIndex(s.get<std::string>()[0]));
+	}
+	for(const auto& p : fetchField(j, "Predicates")) {
+		std::string pred = p.get<std::string>();
+		bool match = false;
+		if (!m_predicates.form) {
+			for(Form f = Form::BEGIN; f != Form::END; f = f+1) {
+				if (pred == toString(f)) {
+					m_predicates.form = f;
+					match = true;
+					break;
+				}
+			}
+		}
+		if (!match && !m_predicates.size) {
+			for(Size s = Size::BEGIN; s != Size::END; s = s+1) {
+				if (pred == toString(s)) {
+					m_predicates.size = s;
+					match = true;
+					break;
+				}
+			}
+		}
+		if(!match) {throw PredicateNotFound(pred); }
+	}
+	auto pos = fetchField(j, "Tags");
+	m_position = vec2(pos[0], pos[1]);
+}
+
--- Field.hpp
+++ Field.hpp
@@ -4,16 +4,65 @@
 #include <string>
 #include <exception>
 #include <stdexcept>
+#include <bitset>
+#include <optional>
+#include <compare>
 
+#include <ncurses.h>
+
+#include <iostream>
+
+#include "json.hpp"
+using json = nlohmann::json;
+
+extern int ColorId;
+
+struct vec2 {
+	vec2(int _x, int _y) : x{_x}, y{_y} {}
+	vec2() = default;
+	int operator[](int i) {
+		if (i == 0) {
+			return x;
+		} else {
+			return y;
+		}
+	}
+	int x = 0;
+	int y = 0;
+	bool operator==(const vec2& _o) {
+		return x == _o.x && y == _o.y;
+	}
+};
+
+struct PredicateNotFound : public std::runtime_error {
+	PredicateNotFound(const std::string& pred)
+		: std::runtime_error{"Predicate not found: "+ pred}{}
+};
 
 static constexpr int FORM_HEIGHT = 4;
 static constexpr int FORM_WIDTH = 9;
-enum class Size { SMALL, MEDIUM, LARGE, END};
+enum class Size { BEGIN, SMALL = BEGIN, MEDIUM, LARGE, END};
+
 constexpr Size operator+(Size s, int i) { return static_cast<Size>(static_cast<int>(s) + i); }
-enum class Form { TET, CUBE, DODEC, END};
+enum class Form { BEGIN, TET = BEGIN, CUBE, DODEC, END};
+extern std::array<int, static_cast<int>(Form::END)> FormColor;
 constexpr Form operator+(Form f, int i) { return static_cast<Form>(static_cast<int>(f) + i); }
-static constexpr std::array<char, 6> symbols =
+
+struct SymbolNotFound : public std::runtime_error {
+	SymbolNotFound(char c) : std::runtime_error(std::string("Symbol not found: ") + c) {}
+};
+
+static constexpr std::array<char, 6> Symbols =
 	{'a', 'b', 'c', 'd', 'e', 'f'};
+inline int getSymbolIndex(char c) {
+	for(std::size_t i = 0; i < Symbols.size(); ++i) {
+		if (Symbols[i] == c) {
+			return i;
+		}
+	}
+	throw SymbolNotFound(c);
+}
+
 
 const char* toString(Size _s);
 const char* toString(Form _f);
@@ -28,41 +77,69 @@ private:
 	}
 };
 
+struct FieldNotFound : public std::runtime_error {
+	FieldNotFound(const char* _name) : std::runtime_error(
+			std::string("Field not found: ") + _name) {}
+};
+
 using form_t = const char[FORM_WIDTH*FORM_HEIGHT + 1];
-namespace hide {
-	template<Size S, Form F>
-	static constexpr form_t form = "";
-
-	template<Size S, Form F = static_cast<Form>(0)>
-	const form_t&
-	getFormForm(Form _f) {
-		if constexpr (F != Form::END) {
-			if (_f == F) {
-				return form<S, F>;
-			} else {
-				return getFormForm<S,F+1>(_f);
-			}
-		} else {
-			throw FormNotFoundException(S, _f);
-		}
-	}
-}
-
-template<Size S = static_cast<Size>(0)>
-const form_t&
-getForm(Size _s, Form _f) {
-	if constexpr (S != Size::END) {
-		if (S == _s) {
-			return hide::getFormForm<S>(_f);
-		} else {
-			return getForm<S+1>(_s, _f);
-		}
-	} else {
-		throw FormNotFoundException(_s, _f);
-	}
-}
 
 
-class Field {
+void initForm();
+
+const form_t& findForm(Size _s, Form _f);
+
+class Object {
 public:
+	Object(const json&);
+	void setForm(Form _f) {
+		m_predicates.form = _f;
+	}
+	const form_t& getForm() const {
+		return ::findForm(m_predicates.size.value(), m_predicates.form.value());
+	}
+	int getColor() const {
+		return FormColor[static_cast<int>(m_predicates.form.value())];
+	}
+	void setSize(Size _s) {
+		m_predicates.size = _s;
+	}
+	bool hasSymbol(int i) const {
+		if (i > m_symbols.size()) { return false; }
+		return m_symbols[i];
+	}
+	int nSymbols() const {
+		return m_symbols.count();
+	}
+
+	void setPosition(const vec2& _pos) {
+		m_position = _pos;
+	}
+	void setPosition(int _x, int _y) {
+		setPosition(vec2(_x, _y));
+	}
+	const vec2& getPosition() const {
+		return m_position;
+	}
+	void setSymbol(int _s, bool _set = true) {
+		m_symbols.set(_s, _set);
+	}
+	auto operator<=>(const Object& oth) {
+		if (auto cmp = m_position.y <=> oth.m_position.y; cmp != 0) {
+			return cmp;
+		} else if (auto cmp = m_position.x <=> oth.m_position.x; cmp != 0){
+			return cmp;
+		}
+		return std::strong_ordering::equal;
+	}
+	auto operator==(const Object& oth) {
+		return m_position == oth.m_position;
+	}
+private:
+	std::bitset<Symbols.size()> m_symbols{0};
+	struct {
+		std::optional<Form> form = std::nullopt;
+		std::optional<Size> size = std::nullopt;
+	} m_predicates;
+	vec2 m_position;
 };
